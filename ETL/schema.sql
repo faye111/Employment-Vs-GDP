6e1a47bce01f971a74090c27ae2f600d3f6b77d9
zareen <zareentazshukur@gmail.com>
2019-08-07 21:21:13 -0500

    updating schema

---
+++ ETL/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE combined_df (
+ Country Varchar(250),
+ Year INT,
+ GDP_Value float,
+ Percentage_of_Employement float
+    );
+	
+DROP TABLE combined_df;
+
+SELECT * FROM combined_df;
