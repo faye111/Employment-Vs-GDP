d80b52f5ed0f592c9c2097b2d637c38bc1e0db49
zareen <zareentazshukur@gmail.com>
2019-08-10 10:33:24 -0500

    adding new tables in schema.sql

--- ETL/schema.sql
+++ ETL/schema.sql
@@ -1,3 +1,5 @@
+--Creating the combined_df table.
+
 CREATE TABLE combined_df (
  Country Varchar(250),
  Year INT,
@@ -5,6 +7,29 @@ CREATE TABLE combined_df (
  Percentage_of_Employement float
     );
 	
-DROP TABLE combined_df;
+--DROP TABLE combined_df;
 
 SELECT * FROM combined_df;
+
+
+--Creating the gdp_df & employ_new_df tables.
+
+CREATE TABLE gdp_df (
+ Country Varchar(250),
+ Year INT,
+ GDP_Value float);
+    
+SELECT * FROM gdp_df;
+
+CREATE TABLE employ_new_df (
+ Country Varchar(250),
+ Year INT,
+ percentage_of_Employement float
+ );
+ 
+ SELECT * FROM employ_new_df;
+
+--Retriving the data based on the year
+
+SELECT * FROM combined_df
+WHERE year=2018;
